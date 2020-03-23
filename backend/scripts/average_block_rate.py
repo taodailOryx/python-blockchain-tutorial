b89072330ad34a54a2477f0944ebf0bd2aab4339
taodailOryx <drew.j.simonson@gmail.com>
2020-03-23 08:51:41 -0400

    add average_block_rate.py to measure mining time

--- backend/scripts/average_block_rate.py
+++ backend/scripts/average_block_rate.py
@@ -1,22 +1,44 @@
-import time
+# import time
 
-from backend.blockchain.blockchain import Blockchain
+# from backend.blockchain.blockchain import Blockchain
+# from backend.config import SECONDS
+
+# blockchain = Blockchain()
+
+# times = []
+
+# for i in range(1000):
+#     start_time = time.time_ns()
+#     blockchain.add_block(i)
+#     end_time = time.time_ns()
+
+#     time_to_mine = (end_time - start_time) / SECONDS
+#     times.append(time_to_mine)
+
+#     average_time = sum(times) / len(times)
+
+#     print(f'New block difficulty: {blockchain.chain[-1].difficulty}')
+#     print(f'Time to mine new block: {time_to_mine}s')
+#     print(f'Average time to add blocks: {average_time}s\n')
+
+import time
 from backend.config import SECONDS
+from backend.blockchain.blockchain import Blockchain
 
 blockchain = Blockchain()
 
 times = []
 
-for i in range(1000):
+for block_count in range(1000):
     start_time = time.time_ns()
-    blockchain.add_block(i)
+    blockchain.add_block(block_count)
     end_time = time.time_ns()
-
     time_to_mine = (end_time - start_time) / SECONDS
     times.append(time_to_mine)
 
     average_time = sum(times) / len(times)
 
-    print(f'New block difficulty: {blockchain.chain[-1].difficulty}')
-    print(f'Time to mine new block: {time_to_mine}s')
-    print(f'Average time to add blocks: {average_time}s\n')
+    print(f'newest block difficulty: {blockchain.chain[-1].difficulty}')
+    print(f'newest block hash: {blockchain.chain[-1].hash}')
+    print(f'time to mine newest block: {time_to_mine}')
+    print(f'current average time to mine a block: {average_time}\n')
